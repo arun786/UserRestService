eef1a4fbfea16164f40b76211ae9e3291fd8e7f4
Arun Singh <arun@Aruns-MacBook-Pro.local>
2020-07-11 22:01:57 -0700

    initial project commit

---
+++ src/main/java/com/arun/userrestservice/UserrestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.arun.userrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserrestserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserrestserviceApplication.class, args);
+    }
+
+}
